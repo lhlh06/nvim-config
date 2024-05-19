be50e29813fd2966a17662eaa36e8de54eac746f
lh <whlbx06@gmail.com>
2024-05-20 00:43:47 +0800

    add tagbar & correct todo-comments & add hotkeys

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -11,4 +11,29 @@ map("n", "j", "v:count == 0 ? 'gj' : 'j'", expr_options)
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 keymap.set("n", "gr", vim.lsp.buf.references)
 
-keymap.set("n", "<leader>fm", ":Format<CR>")
+keymap.set("n", "<leader>fm", ":Format<CR>", { desc = "Format the code" })
+
+
+-- Tabs
+keymap.set("n", "<leader>tc", ":tabnew<CR>", { desc = "create a new tab" })
+keymap.set("n", "<leader>tx", ":tabclose<CR>", { desc = "close the tab" })
+keymap.set("n", "<leader>tn", ":tabn<CR>", { desc = "next tab" })
+keymap.set("n", "<leader>tp", ":tabp<CR>", { desc = "previous tab" })
+
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+
+-- Tagbar
+keymap.set("n", "<leader>ot", "<cmd>TagbarToggle", { desc = "Toggle Tagbar" })
+keymap.set("n", "<leader>on", "<cmd>TagbarJumpNext<CR>", { desc = "Tagbar Next" })
+keymap.set("n", "<leader>op", "<cmd>TagbarJumpPrev<CR>", { desc = "Tagbar Prev" })
+keymap.set("n", "<leader>ox", "<cmd>TagbarClose<CR>", { desc = "Close Tagbar" })
+
+-- buffers
+keymap.set("n", "<leader>bp", "<cmd>bprevious<CR>", { desc = "Close Tagbar" })
+keymap.set("n", "<leader>bn", "<cmd>bnext<CR>", { desc = "Close Tagbar" })
+keymap.set("n", "<leader>bx", "<cmd>bdelete<CR>", { desc = "Close Tagbar" })
--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -1,21 +1,25 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { import = "plugins" },
-  { "ellisonleao/gruvbox.nvim",
+    { import = "plugins" },
+    -- theme
+    {
+        "ellisonleao/gruvbox.nvim",
         priority = 1000,
         config = true,
         opts = ...
-  },
+    },
+    -- TODO: Maybe add Illuminate which is a plugin to automatically highlight
+    -- variable or sth under the curesor with the references.
 })
