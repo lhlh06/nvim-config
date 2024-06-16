5218ec2f0385c77d689dea43e7de5dd6699665a3
lhlh06 <whlbx20124@163.com>
2024-06-16 22:44:02 +0800

    config illuminate style

--- init.lua
+++ init.lua
@@ -5,3 +5,6 @@ require("lazy_setup")
 -- themes
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
+vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#575757" })
+vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#575757" })
+vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#575757" })
--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -1,6 +1,8 @@
-vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#575757" })
-vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#575757" })
-vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#575757" })
 return {
     "RRethy/vim-illuminate",
+    config = function()
+        vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#575757" })
+        vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#575757" })
+        vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#575757" })
+    end
 }
