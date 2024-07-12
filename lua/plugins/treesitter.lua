590649348f743f9c13a7fd6119affb86ab56827b
lh <whlbx06@gmail.com>
2024-07-12 10:56:04 +0800

    add treesitter-textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,12 +4,16 @@ return {
     build = ":TSUpdate",
     dependencies = {
         "windwp/nvim-ts-autotag",
+        "nvim-treesitter/nvim-treesitter-textobjects",
     },
     config = function()
         -- import nvim-treesitter plugin
         local treesitter = require("nvim-treesitter.configs")
         -- configure treesitter
         treesitter.setup({ -- enable syntax highlighting
+            sync_install = false,
+            auto_install = true,
+            ignore_install = {},
             highlight = {
                 enable = true,
             },
@@ -63,6 +67,115 @@ return {
                     node_decremental = "<bs>",
                 },
             },
+
+            textobjects = {
+                select = {
+                    enable = true,
+
+                    -- Automatically jump forward to textobj, similar to targets.vim
+                    lookahead = true,
+
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                        ["ap"] = "@paramerter.outer",
+                        ["ip"] = "@paramerter.inner",
+                        ["ab"] = "@block.outer",
+                        ["ib"] = "@block.inner",
+                    },
+                    -- You can choose the select mode (default is charwise 'v')
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * method: eg 'v' or 'o'
+                    -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                    -- mapping query_strings to modes.
+                    selection_modes = {
+                        ['@parameter.outer'] = 'v', -- charwise
+                        ['@function.outer'] = 'V',  -- linewise
+                        ['@class.outer'] = '<c-v>', -- blockwise
+                    },
+                    -- If you set this to `true` (default is `false`) then any textobject is
+                    -- extended to include preceding or succeeding whitespace. Succeeding
+                    -- whitespace has priority in order to act similarly to eg the built-in
+                    -- `ap`.
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * selection_mode: eg 'v'
+                    -- and should return true or false
+                    include_surrounding_whitespace = true,
+                },
+
+                swap = {
+                    enable = true,
+                    swap_next = {
+                        ["<leader>np"] = "@parameter.inner", -- swap parameters/argument with next
+                        ["<leader>ns"] = "@statement.outer", -- swap object property with next
+                        ["<leader>nm"] = "@function.outer",  -- swap function with next
+                    },
+                    swap_previous = {
+                        ["<leader>pp"] = "@parameter.inner", -- swap parameters/argument with prev
+                        ["<leader>ps"] = "@statement.outer", -- swap object property with prev
+                        ["<leader>pm"] = "@function.outer",  -- swap function with previous
+                    },
+                },
+
+                move = {
+                    enable = true,
+                    set_jumps = true,
+                    goto_next_start = {
+                        ["]f"] = { query = "@call.outer", desc = "Next function call start" },
+                        ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
+                        ["]c"] = { query = "@class.outer", desc = "Next class start" },
+                        ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
+                        ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
+
+                        -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+                        -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+                        ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+                        ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+                    },
+                    goto_next_end = {
+                        ["]F"] = { query = "@call.outer", desc = "Next function call end" },
+                        ["]M"] = { query = "@function.outer", desc = "Next method/function def end" },
+                        ["]C"] = { query = "@class.outer", desc = "Next class end" },
+                        ["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
+                        ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
+                    },
+                    goto_previous_start = {
+                        ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
+                        ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
+                        ["[c"] = { query = "@class.outer", desc = "Prev class start" },
+                        ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
+                        ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
+                    },
+                    goto_previous_end = {
+                        ["[F"] = { query = "@call.outer", desc = "Prev function call end" },
+                        ["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
+                        ["[C"] = { query = "@class.outer", desc = "Prev class end" },
+                        ["[I"] = { query = "@conditional.outer", desc = "Prev conditional end" },
+                        ["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
+                    },
+
+                }
+
+            },
         })
+        local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+
+        -- vim way: ; goes to the direction you were moving.
+        vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+        vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+
+        -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+        vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+        vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+        vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+        vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
     end,
 }
