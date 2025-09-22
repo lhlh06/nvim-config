a29ac58c70e9409512c82f3c7bb1f8930a009cb7
lhlh06 <whlbx20124@163.com>
2025-09-22 22:20:50 +0200

    use `snacks` input instead of `dressing` & enable `snacks` dashboard

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,55 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        bigfile = { enabled = false },
+        dashboard = {
+            enabled = true,
+            sections = {
+                { section = "header" },
+                {
+                    pane = 2,
+                    section = "terminal",
+                    -- cmd = "colorscript -e square",
+                    height = 5,
+                    padding = 1,
+                },
+                { section = "keys", gap = 1, padding = 1 },
+                { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+                { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+                {
+                    pane = 2,
+                    icon = " ",
+                    title = "Git Status",
+                    section = "terminal",
+                    enabled = function()
+                        return Snacks.git.get_root() ~= nil
+                    end,
+                    cmd = "git status --short --branch --renames",
+                    height = 5,
+                    padding = 1,
+                    ttl = 5 * 60,
+                    indent = 3,
+                },
+                { section = "startup" },
+            },
+        },
+        explorer = { enabled = false },
+        indent = { enabled = false },
+        input = {
+            enabled = true,
+        },
+        picker = { enabled = false },
+        notifier = { enabled = false },
+        quickfile = { enabled = false },
+        scope = { enabled = false },
+        scroll = { enabled = false },
+        statuscolumn = { enabled = false },
+        words = { enabled = false },
+    },
+}
