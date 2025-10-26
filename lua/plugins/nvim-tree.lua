88cabbc7d462189eb903dd680d36aa1d72d40e8d
lhlh06 <whlbx20124@163.com>
2025-10-26 20:32:00 +0100

    fix: `.cache` excluded corretly

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -38,7 +38,7 @@ return {
                 },
             },
             filters = {
-                custom = { ".DS_Store", ".git", ".cache", "node_modules", "*.d$", "*.o$" },
+                custom = { ".DS_Store", ".git", "^.cache", "node_modules", "*.d$", "*.o$" },
             },
             git = {
                 ignore = false,
