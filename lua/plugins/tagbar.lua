16b9e5743b263da7c318334f8aa4bcfe0ecfb139
Xu <whlbx20124@163.com>
2025-12-29 20:25:37 +0100

    fix: hotkey

--- lua/plugins/tagbar.lua
+++ lua/plugins/tagbar.lua
@@ -3,9 +3,9 @@ return {
 	cmd = { "TagbarToggle", "TagbarOpen", "TagbarClose" },
 	keys = {
 		{ "<leader>ot", "<cmd>TagbarToggle<cr>", desc = "Toggle Tagbar" },
-		{ "n", "<leader>ot", "<cmd>TagbarToggle<CR>", desc = "Toggle Tagbar" },
-		{ "n", "<leader>on", "<cmd>TagbarJumpNext<CR>", desc = "Tagbar Next" },
-		{ "n", "<leader>op", "<cmd>TagbarJumpPrev<CR>", desc = "Tagbar Prev" },
-		{ "n", "<leader>ox", "<cmd>TagbarClose<CR>", desc = "Close Tagbar" },
+		{ "<leader>ot", "<cmd>TagbarToggle<CR>", desc = "Toggle Tagbar" },
+		{ "<leader>on", "<cmd>TagbarJumpNext<CR>", desc = "Tagbar Next" },
+		{ "<leader>op", "<cmd>TagbarJumpPrev<CR>", desc = "Tagbar Prev" },
+		{ "<leader>ox", "<cmd>TagbarClose<CR>", desc = "Close Tagbar" },
 	},
 }
