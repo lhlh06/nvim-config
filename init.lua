23465fa4a2d2b55cf2058fb7613c8262819c70b2
lhlh06 <whlbx20124@163.com>
2026-02-16 12:20:19 +0800

    underline rust mutable vars and methods

--- init.lua
+++ init.lua
@@ -9,3 +9,5 @@ require("custom")
 -- themes
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
+-- underline rust mutable vars and methods
+vim.api.nvim_set_hl(0, "@lsp.mod.mutable.rust", { underline = true })
