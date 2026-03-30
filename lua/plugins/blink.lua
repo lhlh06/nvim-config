ec5d561f39ac212e441919d8132b75ee6e4338a9
Xu <whlbx20124@163.com>
2026-03-30 14:32:23 +0200

    remove redundant configs

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,51 +1,58 @@
 return {
-    'saghen/blink.cmp',
-    -- optional: provides snippets for the snippet source
-    dependencies = {
-        'rafamadriz/friendly-snippets',
-        {
-            'L3MON4D3/LuaSnip',
-            version = 'v2.*',
-        }
-    },
-    version = '1.*',
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-        keymap = {
-            preset = 'none',
-            ['<Enter>'] = { 'select_and_accept', 'fallback' },
-            -- ['<Tab>'] = {},
-            ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-            ['<C-e>'] = { 'hide' },
-            ['<C-y>'] = { 'select_and_accept' },
+	"saghen/blink.cmp",
+	-- optional: provides snippets for the snippet source
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		{
+			"L3MON4D3/LuaSnip",
+			version = "v2.*",
+		},
+	},
+	-- TODO: wait until stable V2
+	version = "1.*",
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		keymap = {
+			preset = "none",
+			["<Enter>"] = { "select_and_accept", "fallback" },
+			-- ['<Tab>'] = {},
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide" },
+			["<C-y>"] = { "select_and_accept" },
 
-            ['<Up>'] = { 'select_prev', 'fallback' },
-            ['<Down>'] = { 'select_next', 'fallback' },
-            ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
-            ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+			["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+			["<C-n>"] = { "select_next", "fallback_to_mappings" },
 
-            ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-            ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
 
-            ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
-        },
-        appearance = {
-            nerd_font_variant = 'mono'
-        },
-        completion = {
-            documentation = { auto_show = true, auto_show_delay_ms = 500 },
-            ghost_text = {
-                enabled = true,
-            },
+			["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+		},
+        -- TODO: wait to stable
+		-- signature = {
+		-- 	enabled = true,
+		-- 	window = {
+		-- 		show_documentation = true,
+		-- 	},
+		-- },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+		completion = {
+			documentation = { auto_show = true, auto_show_delay_ms = 500 },
+			ghost_text = {
+				enabled = true,
+			},
+		},
+		snippets = { preset = "luasnip" },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
 
-        },
-        snippets = { preset = 'luasnip' },
-        sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
-        },
-
-        fuzzy = { implementation = "prefer_rust_with_warning" }
-    },
-    opts_extend = { "sources.default" }
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+	},
+	opts_extend = { "sources.default" },
 }
--- lua/plugins/tagbar.lua
+++ lua/plugins/tagbar.lua
@@ -2,7 +2,6 @@ return {
 	"preservim/tagbar",
 	cmd = { "TagbarToggle", "TagbarOpen", "TagbarClose" },
 	keys = {
-		{ "<leader>ot", "<cmd>TagbarToggle<cr>", desc = "Toggle Tagbar" },
 		{ "<leader>ot", "<cmd>TagbarToggle<CR>", desc = "Toggle Tagbar" },
 		{ "<leader>on", "<cmd>TagbarJumpNext<CR>", desc = "Tagbar Next" },
 		{ "<leader>op", "<cmd>TagbarJumpPrev<CR>", desc = "Tagbar Prev" },
